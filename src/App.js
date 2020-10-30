0566d429c4052b42a59c7579bd2f6ee6196b821a
edwardcashmere <eomondi06@gmail.com>
2020-10-30 12:54:46 +0300

    few

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Still Under Construction
         </a>
       </header>
     </div>
